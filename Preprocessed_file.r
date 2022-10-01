56c35aa865009fe4726269e6a37d6cb9240361ba
Julia <julia.jose2402@gmail.com>
2022-10-01 18:53:49 +0100

    finished matrix T, matrix A and vector S

--- Preprocessed_file.r
+++ Preprocessed_file.r
@@ -1,4 +1,6 @@
 setwd("C:\\Users\\julia\\Desktop\\Julia_Git_repo\\StatisticalProgramming")
+  
+start_time= Sys.time()
 a <- scan("pg10.txt",what="character",skip=104)
 n <- length(a)
 a <- a[-((n-2886):n)]
@@ -38,4 +40,55 @@ head(sort(tab,decreasing=TRUE),n=500)
 b = (unique_words[tab>=163])
 b 
 
+# -------- Task 7: Generating matrix T -------------------
 
+# (a)
+index_vector_freq=match(tolower(a),b)
+index_vector_freq[0:20]
+
+vec2 = index_vector_freq[2:length(index_vector_freq)] |> append(NA)
+vec3 = index_vector_freq[3:length(index_vector_freq)] |> append(NA) |> append(NA)
+
+col_matrix=cbind(index_vector_freq,vec2,vec3)
+
+col_matrix_T= col_matrix[is.na(rowSums(col_matrix))== FALSE,1:3]
+
+# b may contain words dropped after NA, just using it for now
+T_matrix = array(0,dim=c(500, 500,500))
+T_matrix
+
+for (row in 1:length(col_matrix_T[,1])){
+  current_row=col_matrix_T[row,]
+  i=current_row[1]
+  j=current_row[2]
+  k=current_row[3]
+  T_matrix[i,j,k]=T_matrix[i,j,k]+1
+}
+
+T_matrix[105,2,107]
+
+# --------- Creating matrix A (for pairs of words)---------
+
+col_matrix_A= cbind(index_vector_freq,vec2)
+col_matrix_A
+
+col_matrix_A= col_matrix_A[is.na(rowSums(col_matrix_A))== FALSE,]
+
+A_matrix = array(0,dim=c(500,500)) #Should it be 500 or something else?
+A_matrix
+
+for (row in 1:length(col_matrix_A[,1])){
+  current_row=col_matrix_A[row,]
+  i=current_row[1]
+  j=current_row[2]
+  A_matrix[i,j]=A_matrix[i,j]+1
+}
+
+# ------------ Creating vector S
+vector_S = tabulate(index_vector_freq)
+
+# --------------Simulation
+end_time=Sys.time()
+totaltime=end_time-start_time
+
+totaltime
