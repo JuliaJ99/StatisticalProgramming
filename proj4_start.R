b92e0652fb6232aad09e8b7bf040aa8cfd88311f
a-ishiyama <akira.j.ishiyama@gmail.com>
2022-11-15 01:31:22 +0000

    added comments for the code, 3-4 lines needs update

--- proj4_start.R
+++ proj4_start.R
@@ -1,131 +1,119 @@
-
-# ** Julia J, 14/11 morning updates:
-# Made a few changes to pd check and pd fix
-# Made sure the 'max iter without convergence' message appears
-# Still not sure of the convergence formula. By tweaking it, either we reach convergence too early around 1.9-1.3, 
-# or we don't converge at all
-
-# Please don't remove any of the print statements yet. I know it's annoying but it's useful to observe the process
-
-
-newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,maxit=100,max.half=20,eps=1e-6){
-  # if hess not supplied use the finite difference approximation
+newt <- function(theta, func, grad, hess = NULL, ..., tol = 1e-8, fscale = 1, 
+                 maxit = 100, max.half = 20, eps = 1e-6){
+#!! Explain what newt does
   
-  if (missing(hess)){ #*** Julia J: replaced ==Null with missing
-    hess <- function(grad,theta,eps,...){
-      grad_th <- grad(theta)
-      Hfd <- matrix(0,length(theta),length(theta))
-      
-      cat('entered hessian define func')
+  # Finite difference approximation for the Hessian matrix if the 'hess'
+  # argument is missing
+  if (missing(hess)){
+    hess <- function(grad, theta, eps, ...){
+      Hfd <- matrix(0, length(theta), length(theta))
+      # generate a matrix of 0s to later replace the values with the 
+      # second derivatives
       for (i in 1:length(theta)){
-        new_theta <- theta 
+        new_theta <- theta
         new_theta[i] <- new_theta[i] + eps
-        grad_new <- grad(new_theta)
-        Hfd[i,] <- (grad_new - grad_th)/eps
+        Hfd[i,] <- (grad(new_theta, ...) - grad(theta, ...)) / eps
+        # this step essentially computes the second derivatives. 
+        # second derivatives can be obtained by applying the first principle to
+        # 'grad', the first derivatives.
       }
-      
-      (t(Hfd)+Hfd)/2
+      (t(Hfd) + Hfd) / 2
+      #!! how should i explain this step
     }
   }
   
-  # checking if objective or derivatives are finite at the initial theta 
-  if ((abs(func(theta))==Inf) || 
-      any(abs(grad(theta))==Inf) || 
-      any(c(abs(hess(theta)))==Inf)) { # should hess be included?
-    stop('Objective function or derivatives not finite at the initial theta!') #stop or warning?
+  # Break the function if any one of the objectives or derivatives are not 
+  # finite at the initial theta
+  if ((abs(func(theta)) == Inf) || 
+      any(abs(grad(theta)) == Inf) || 
+      any(c(abs(hess(theta))) == Inf)) {
+    stop('Objective function or derivatives not finite at the initial theta')
+  }
+  
+  #!! How should i explain this step
+  pd_fix<- function(hess_th){
+    while(inherits(try(chol(hess_th), silent = TRUE), "try-error")){
+      hess_th <- hess_th + diag(dim(hess_th)[1])
+    }
+    hess_th
   }
   
   new_theta <- theta
-  iter <- 0
+  iter <- 0 # the number of iteration done in the while loop
   
+  # Loop to search for the theta that minimises the objective function
   while (iter <= maxit){
-    cat('\n\nNew Iteration',iter)
-    cat('\n Theta start of loop',new_theta)
     hess_th <- hess(new_theta)
-    hess_th<-pd_fix(hess_th)
+    # second derivatives
+    hess_th <- pd_fix(hess_th)
     grad_th <- grad(new_theta)
-    old_D <- func(new_theta)
+    # first derivatives
+    D <- func(new_theta)
+    # objective function values
     chol_hess_th <- chol(hess_th)
-    cat('\n func value at old_theta',old_D)
-    delta <- -1*(Matrix::chol2inv(chol_hess_th))%*%grad_th
-
-    new_D <- old_D+ (t(delta)%*%grad_th)+ (0.5*t(delta)%*%hess_th%*%delta) #or new_D = D(theta+delta)
+    # Cholesky decomposition is a preparatory step towards using chol2inv in
+    # the next line. it computes the inverse much more efficiently compared to
+    # 'solve' function
+    delta <- -1 * (Matrix::chol2inv(chol_hess_th)) %*% grad_th
+    # find the inverse of 'hess_th', the Hessian given the theta, and multiply
+    # it with -1 and 'grad_th', the first derivative, to compute 'delta' that 
+    # minimises the D_delta below, based on Taylor's theorem
+    D_delta <- func(new_theta + delta)
+    # objective function values updated with delta
     
-    # new_D<-func(new_theta+delta)
-    
-    step <- 0 # number of times we halve delta
-    while (new_D > old_D){
+    step <- 0 # the number of times delta is halved
+    while (D_delta > D){
       step <- step + 1
+      
+      # Break the function if the number of steps halved goes above the 
+      # number set by 'max.half'
       if (step > max.half){
-        stop('Step fails to reduce!')
+        stop('Step fails to reduce')
       }
-      delta <- delta/2
-      new_D <- old_D+ (t(delta)%*%grad_th )+ (0.5*t(delta)%*%hess_th%*%delta) #or new_D = D(theta+delta)
-      # new_D<-func(new_theta+delta)
-      cat('\nEntered step halving, new_D with new halved delta is ',new_D)
+      
+      delta <- delta / 2
+      D_delta <- func(new_theta + delta)
+      # update objective function values to test whether it is now smaller 
+      # than D, the condition set in the while loop, after delta being halved
     }
     
-    cat('\n func value at new_theta',new_D)
-    
     new_theta <- new_theta + delta
-    cat('\n theta + delta',new_theta)
-    convergence <- tol*(abs(new_D)+fscale)#brackets or no brackets???
-    cat('\n Convergence value',convergence)
-    cat('\n Grad value',grad(new_theta))
-    if (all(abs(grad(new_theta)) < rep(convergence,times=length(grad(new_theta))))){
+    # optimal theta obtained by the loop above that minimises the objective 
+    # function value
+    convergence <- tol * (abs(D_delta) + fscale)
+    #!! need explaination?
+    
+    
+    # Examine if the gradient of the objective function at new_theta is 
+    # approximately 0, after taking into account of the tolerance set by the 
+    # function user.
+    if (all(abs(grad(new_theta)) < rep(convergence,
+                                       times = length(grad(new_theta))))){
       
-      # ** Julia J: The documentation for 'try' suggests using inherits for this kind of situation
-      
-      if (inherits(try(chol(hess(new_theta)),silent=TRUE),"try-error")){
-        stop('Hessian not positive definite at convergence!') # stop or warning?
+      # Break the function if the hessian is not positive definite at 
+      # convergence
+      if (inherits(try(chol(hess(new_theta)), silent = TRUE), "try-error")){
+        stop('Hessian not positive definite at convergence')
       }
       
-      #converged - output the optimized theta
-      cat('\n Convergence reached!')
+      # Convergence achieved, return the results
+      cat('Convergence reached')
       return(list('f' = func(new_theta), 'theta' = new_theta, 
                   'iter' = iter, 'g' = grad(new_theta), 
                   'Hi' = chol2inv(chol(hess(new_theta)))))
+      
     }
     
     iter <- iter + 1
   }
   
-  # max number of iterations
-    if (iter >= maxit){
-      cat('Maximum number of iterations reached without convergence!')
-    }
-  
-  #check convergance
-  
-  
-} #end of the newt function
-
-# 
-# # checking if hessian is positive definite
-# pd_check<- function(hess_th){
-#  
-#   if (class(try(chol(hess_th),silent=TRUE))=='try-error'){
-#     stop('Hessian not positive definite!')
-#   }
-# }
-
-# fixing a non-pd hessian
-# this fixes the hessian, but idk how we should adjust the values of theta/grad/D
-# to the new hessian
-pd_fix<- function(hess_th){
-  
-  # ** Julia J: The documentation for 'try' suggests using inherits for this kind of situation
-  
-  while(inherits(try(chol(hess_th),silent=TRUE),"try-error")){
-    hess_th <- hess_th + diag(dim(hess_th)[1]) # diagonal should be multipled 
-                              # by some value, i chose to do a while loop for the
-                              # multiple but idk if it works for small values for example
+  # If the iteration count reaches to be greater than or equal to the number 
+  # set by 'maxit', notify the user that convergence was not reached
+  if (iter >= maxit){
+    cat('Maximum number of iterations reached without convergence')
   }
-  return(hess_th)
+  
 }
-
-
-
 ## Testing---------------------------------------------------------------------
 
 rb <- function(th,k=2) {
@@ -144,20 +132,5 @@ hb <- function(th,k=2) {
 
 th <-c(1,1)
 
-# hb(th)
-# A <-hess(gb,th,eps=1e-6,th=th,k=2)
-
 newt(theta=c(1,3),rb,gb,hb)
-
-# fOR TOMO:
-# library(debug)
-# mtrace(newt)
-# newt(theta=c(1,3),rb,gb,hb)
-# mtrace(newt)
-# Q
-# all(c(1,1)<c(2,2))
-
-A<- matrix(1:4,nrow=2,ncol=2)
-length(class(try(chol(A),silent=TRUE))=='try-error')
-inherits(try(chol(A),silent=TRUE),"try-error")
 
